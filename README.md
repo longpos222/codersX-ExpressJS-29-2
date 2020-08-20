4c88eebe4496c887affb38b255513ea8fdf77c6b
j <longnguyen16.ck27@st.ueh.edu.vn>
2020-08-20 21:09:38 +0700

    Done Lesson 08-3

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # codersX-ExpressJS-08-2
-### BÀI 2
-+ Sử dụng Router như đã học ở trong bài để update lại code của bạn
-
+### BÀI 3
+Bạn chợt nhận ra là giả sử bây giờ có một người đến thuê, làm thế nào để biết ai đang thuê cuốn nào? Bạn cần phải có thêm 1 collection chứa các transaction của việc thuê sách. Mỗi object trong collection này sẽ chứa: userId, bookId, tất nhiên chúng sẽ có 1 property id riêng của mình.
+1) Update lại file db json của bạn
+2) Thêm route /transactions hiển thị các transactions đã tạo. 3) Thêm trạng /transactions/create chứa form gồm 2 field là 2 dropdown (sử dụng select và option để giải quyết). Một dropdown dành cho các user, 1 dropdown dành cho việc chọn sách. Đừng quên nút Create để tạo mới.
+4) Action của form trên có thể để /transactions/create (hoặc để trống sẽ tự hiểu là POST lên URL hiện tại) và method là POST (of course)
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const userRoute = require('./routers/user.route.js');
-const bookRoute = require('./routers/book.route.js');
 const indexRoute = require('./routers/index.route.js');
+const bookRoute = require('./routers/book.route.js');
+const transactionRoute = require('./routers/transaction.route.js');
+const userRoute = require('./routers/user.route.js');
 
 const app = express();
 const port = 3000;
 
-app.set('views', './views');
+app.set('views','./views');
 app.set('view engine','pug');
 
 app.use(bodyParser.json());
@@ -16,8 +17,9 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 app.use('/', indexRoute);
 app.use('/books', bookRoute);
+app.use('/transactions', transactionRoute);
 app.use('/users', userRoute);
 
 app.listen(port, () => {
-console.log(`Server is running at http://localhost:${port}/`);
+console.log(`Server is running at localhost:${port}`);
 });
